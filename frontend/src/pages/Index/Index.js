25cbb41ca42bb85ce8fe484e2cff71438eb77bed
Hossein Salehi <90480508+hossein003@users.noreply.github.com>
2024-03-04 12:33:35 +0330

    folder-structure

---
+++ frontend/src/pages/Index/Index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './Index.css'
+
+export default function index() {
+  return (
+    <div>index</div>
+  )
+}
---
+++ frontend/src/pages/ProductInfo/ProductInfo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './ProductInfo.css'
+
+export default function ProductInfo() {
+  return (
+    <div>ProductInfo</div>
+  )
+}
